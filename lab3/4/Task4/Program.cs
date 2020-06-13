09f3789e827613ebe0c220ed85bae0c61c30dd34
zhekf1 <30550350+zhekf1@users.noreply.github.com>
2020-06-13 12:17:54 +0300

    Add files via upload

---
+++ lab3/4/Task4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char? buf = null;
+            int counter = 0;
+            string line; 
+            char[] separator = { ' ', '.', '*', '+', ';', ',', '?', '!', '‐', '/' };
+            StringBuilder answer = new StringBuilder();
+            Console.WriteLine("Reading file:");
+            using (StreamReader File = new StreamReader("text.txt"))
+            {
+                while ((line = File.ReadLine()) != null)
+                {
+                    Console.WriteLine(line); 
+                    string[] words = line.Split(separator);
+                    foreach (string word in words) 
+                    {
+                        if (buf == null)
+                        {
+                            buf = word[word.Length - 1];
+                        }
+                        
+                        else if (!string.IsNullOrWhiteSpace(word) && 
+                                 word.All(s => char.IsLetter(s)) && word[word.Length - 1] == buf)
+                        {
+                            counter ++;
+                            answer.Append(word).Append(" ");
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("There were {0} sequences.", counter);
+            Console.WriteLine(answer.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab3/5/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String one = "Hello ";
+            String two = "World ";
+            Console.WriteLine(string.Concat(one, two));
+            var inserted = one.Insert(one.Length - 1, two);
+            Console.WriteLine(inserted);
+            Console.WriteLine(one.Replace('l','r'));
+            Console.WriteLine(String.Compare(one, two));
+            Console.WriteLine(one.IndexOf("el"));
+        }
+    }
+}
